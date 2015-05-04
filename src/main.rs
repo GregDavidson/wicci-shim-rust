841287a76ae556560c5ae33f539fd7e63bf601fb
GregDavidson <greg.davidson+git@gmail.com>
2015-05-03 21:58:24 -0700

    getting options now works

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,19 @@ extern crate lazy_static;
 extern crate getopts;
 // use getopts::Options;
 use std::env;
-use std::sync::{Once, ONCE_INIT};
 
-type str_vec = Vec<String>;
-// type str_vec = Vec<&'static str>;
-fn pgm_args() -> &str_vec {
-    static once: Once = ONCE_INIT;
-    static mut argv: str_vec;
-    once.call_once( | | argv = *env::args().collect() );
-    &argv
-}
-fn pgm_name() -> String { pgm_args()[0] }
+lazy_static! {
+    static ref PGM_ARGV: Vec<String> = {
+        // let mut argv = Vec::new();
+        // argv = env::args().collect();
+        let argv = env::args().collect();
+        argv
+    };
+    static ref PGM_NAME: String = PGM_ARGV[0].clone();
+    static ref PGM_ARGS: &'static[String] = &PGM_ARGV[1..];
 
-fn get_options(argv: &[String])->getopts::Options {
-    static once: Once = ONCE_INIT;
-    static mut opts;
-    once.call_once( || {
-        opts = getopts::Options::new();
+    static ref PGM_OPTIONS: getopts::Options = {
+        let mut opts = getopts::Options::new();
         opts.optflag("h", "help", "print this help menu");
         opts.optflag("d", "debug", "trace what's going on");
         opts.optflag("D", "show-args", "show program arguments");
@@ -35,43 +31,24 @@ fn get_options(argv: &[String])->getopts::Options {
         opts.optopt("", "user", "", "");
 	      opts.optopt("", "password", "", "");
 	      opts.optopt("", "connect_timeout", "", "");
-    } );
-    opts
-}
-fn get_matches(argv: &[String], opts: getopts::Options)
-->getopts::Matches {
-    static once: Once = ONCE_INIT;
-    let matches;
-    once.call_once( | | {
-        matches = match get_options(argv).parse( argv ) {
+        opts
+    };
+    
+    static ref PGM_OPTS: getopts::Matches
+        = match PGM_OPTIONS.parse( PGM_ARGS.iter() ) {
             Ok(m) => m,
             Err(f) => {
-                env::set_exit_status(1);
+                // env::set_exit_status(1);
                 panic!(f.to_string());
             }
-        };
-    } );
-    matches
-}
+    };
+}                              // lazy_static
 
-fn print_usage(opts: &getopts::Options) {
-    let brief = format!("Usage: {} [options]...", pgm_name());
-    print!("{}", opts.usage(&brief));
-}
-
-type MatchesBox = Box<getopts::Matches>;
-fn pgm_opts() -> getopts::Matches {
-    static once: Once = ONCE_INIT;
-    let mut opts: MatchesBox;
-    once.call_once( | | opts = Box::new( {
-        let args = &pgm_args()[1..];
-        get_matches(&args, get_options( args ) )
-    } ) );
-    *opts
+fn print_usage() {
+    let brief = format!("Usage: {} [options]...", *PGM_NAME);
+    print!("{}", PGM_OPTIONS.usage(&brief));
 }
 
 fn main() {
-    if pgm_opts().opt_present("help") {
-        print_usage( &get_options(&pgm_args()[1..]) )
-    }
+    if PGM_OPTS.opt_present("help") { print_usage() };
 }
